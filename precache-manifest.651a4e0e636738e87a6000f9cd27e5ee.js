aeee8cda4b3d88c758a5f64cb0a2c342e6d2d4d8
sgranger2 <shaneg457@gmail.com>
2019-01-21 19:24:56 -0600

    Updates

---
+++ precache-manifest.651a4e0e636738e87a6000f9cd27e5ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "16a7186d7826cab35cd8f7d3c224b391",
+    "url": "/bodyworkz/static/media/gym.16a7186d.jpg"
+  },
+  {
+    "revision": "5d63d2711d621bf7d810977c363b5b2c",
+    "url": "/bodyworkz/static/media/brux.5d63d271.ttf"
+  },
+  {
+    "revision": "9f25b1f8c62ddd2ad699a89aaaf11a59",
+    "url": "/bodyworkz/static/media/HelveticaNeueMed.9f25b1f8.ttf"
+  },
+  {
+    "revision": "d59ec4ec7314e158039d",
+    "url": "/bodyworkz/static/js/runtime~main.d59ec4ec.js"
+  },
+  {
+    "revision": "215b32e50a0807f65305",
+    "url": "/bodyworkz/static/js/main.215b32e5.chunk.js"
+  },
+  {
+    "revision": "e73bbedd0c6746eca063",
+    "url": "/bodyworkz/static/js/1.e73bbedd.chunk.js"
+  },
+  {
+    "revision": "215b32e50a0807f65305",
+    "url": "/bodyworkz/static/css/main.9dbe2d9b.chunk.css"
+  },
+  {
+    "revision": "f69444fb2c2acb798110a133710c1dba",
+    "url": "/bodyworkz/index.html"
+  }
+];
