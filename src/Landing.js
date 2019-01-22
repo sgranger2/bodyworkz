1aea76a48bed844c108c9524626f0336f8ad18ca
sgranger2 <shaneg457@gmail.com>
2019-01-22 14:33:39 -0600

    changed the hook text

--- src/Landing.js
+++ src/Landing.js
@@ -3,33 +3,38 @@ import './App.css';
 
 class Landing extends React.Component {
 
-    
+
 
     render() {
-         return (
-        <header className="App-header">
-            <nav className="nav-bar">
-                <h3>Body Workz</h3>
-                <div>
-                    <ul>
-                        <li>About Us</li>
-                        <li>Schedule</li>
-                        <li>Trainers</li>
-                        <li>Rates</li>
-                        <li>Contact</li>
-                    </ul>
+        return (
+            <header className="App-header">
+                <nav className="nav-bar">
+                    <h3>Body Workz</h3>
+                    <div>
+                        <ul>
+                            <li>About Us</li>
+                            <li>Schedule</li>
+                            <li>Trainers</li>
+                            <li>Rates</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className="header-hook">
+                    <p>Helping You become your</p>
+                    <div>
+                        <p className="emphasis">Best</p>
+                        <p className="emphasis">Self</p>
+                    </div>
                 </div>
-            </nav>
-            <div className="header-hook">
-                <p>Helping You become your best self</p>
-            </div>
-            <div className="header-bottom">
-                <p>Orange and Bridge City</p>
-            </div>
-        </header>
-    );
+                <div></div>
+                <div className="header-bottom">
+                    <p>Orange and Bridge City</p>
+                </div>
+            </header>
+        );
     }
-   
+
 }
 
 export default Landing;
